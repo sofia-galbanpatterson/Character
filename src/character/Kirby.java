3d05fa12fd930727baa69040edc70a472c736fad
Sofia <sofialuzgalbanpatterson@Sofias-MacBook-Pro.local>
2015-11-03 10:10:37 -0800

    Character Update

--- src/character/Kirby.java
+++ src/character/Kirby.java
@@ -23,7 +23,9 @@ public class Kirby {
 //        Graphics2D g2d = (Graphics2D) graphics; 
 //        g2d.rotate(Math.toRadians(45));
 //        g2d.fillOval(x + 150 , y - 200, width / 2, , height /2);  
-//        g2d.rotate(Math.toRadians(-45));  
+//        g2d.rotate(Math.toRadians(-45)); 
+        
+        
         graphics.setColor(Color.red);
         graphics.fillOval(x + (width * 10 / 100), y + (height * 20 / 100), width / 2, height / 2);
 
@@ -31,43 +33,43 @@ public class Kirby {
         graphics.setColor(MY_PINK);
         graphics.fillOval(x, y, width, height);
 
-        //eyes
+        //eyes  
         graphics.setColor(Color.BLACK);
-        graphics.fillOval(x + (width * 35 / 100), y + (height * 40 / 100), width * 10 / 100, height * 20 / 100);
-        graphics.fillOval(x + (width * 60 / 100), y + (height * 40 / 100), width * 10 / 100, height * 20 / 100);
-
-        //eye reflections
-        graphics.setColor(Color.WHITE);
-        graphics.fillOval(x + (width * 37 / 100), y + (height * 40 / 100), width * 5 / 100, height * 10 / 100);
-        graphics.fillOval(x + (width * 60 / 100), y + (height * 40 / 100), width * 5 / 100, height * 10 / 100);
-
-        graphics.setColor(Color.BLACK);
-        graphics.fillOval(540, 310, 40, 80);
-        graphics.fillOval(640, 310, 40, 80);
-
-        graphics.setColor(Color.white);
-        graphics.fillOval(555, 310, 20, 40);
-        graphics.fillOval(655, 310, 20, 40); 
+        graphics.fillOval(x + (width * 35 / 100), y + (height * 30 / 100), width * 10 / 100, height * 20 / 100);
+        graphics.fillOval(x + (width * 60 / 100), y + (height * 30 / 100), width * 10 / 100, height * 20 / 100);
 
         // blue eye color 
         graphics.setColor(new Color(37, 50, 244));
-        graphics.fillOval(x + (width * 40 / 100), y + (height * 53 / 100), width * 5 / 100, height * 10 / 100);
+        graphics.fillOval(x + (width * 36 / 100), y + (height * 30 / 100), width * 8 / 100, height * 15 / 100);
+        graphics.fillOval(x + (width * 61 / 100), y + (height * 30 / 100), width * 8 / 100, height * 15 / 100);
 
-        graphics.setColor(new Color(37, 50, 244));
-        graphics.fillOval(540, 310, 20, 40);
-        graphics.fillOval(655, 310, 20, 40);
+        //eye reflections
+        graphics.setColor(Color.WHITE);
+        graphics.fillOval(x + (width * 37 / 100), y + (height * 30 / 100), width * 5 / 100, height * 10 / 100);
+        graphics.fillOval(x + (width * 62 / 100), y + (height * 30 / 100), width * 5 / 100, height * 10 / 100);
 
+        // pink cheeks
         graphics.setColor(new Color(251, 20, 170));
         graphics.fillOval(588, 450, 40, 30);
+        graphics.fillOval(x + (width * 30 / 100), y + (height * 50 / 100), width * 10 / 100, height * 5 / 100);
+        graphics.fillOval(x + (width * 66 / 100), y + (height * 50 / 100), width * 10 / 100, height * 5 / 100);
+        
+        // mouth 
+        graphics.setColor(new Color(168, 48, 48));
+        graphics.fillOval(600, 200, 40, 40);
+        
+        
+        
 
-        // pink cheeks 
-        graphics.fillOval(545, 400, 40, 20);
-        graphics.fillOval(645, 400, 40, 20);
-
-        // legs 
-        graphics.setColor(Color.RED);
-        graphics.fillOval(400, 500, 100, 150);
-
+//        graphics.setColor(Color.BLACK);
+//        graphics.fillOval(540, 310, 40, 80);
+//        graphics.fillOval(640, 310, 40, 80);
+//        graphics.setColor(Color.white);
+//        graphics.fillOval(555, 310, 20, 40);
+//        graphics.fillOval(655, 310, 20, 40); 
+//        graphics.setColor(new Color(37, 50, 244));
+//        graphics.fillOval(540, 310, 20, 40);
+//        graphics.fillOval(655, 310, 20, 40);
     }
 
     public Kirby(int x, int y, int height, int width) {
